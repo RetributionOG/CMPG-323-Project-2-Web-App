9f14189d4d1d0a3cb144bef502f5e92657b34e6e
RetributionOG <cameronkdb@gmail.com>
2021-11-08 22:17:09 +0200

    Login page created and DB is setup

---
+++ MemShare/Login.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="MemShare.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Container-sm/OverallStyleSheet.css" rel="stylesheet" />
+    <style type="text/css">
+        .btnSign {
+            z-index: 1;
+            left: 41px;
+            top: 381px;
+            position: absolute;
+        }
+    </style>
+</head>
+<body>
+    <div class="loginbox">
+        <form runat="server">
+            &nbsp;
+            <asp:Label ID="lblError" runat="server" BackColor="Red" ForeColor="#FFD909" style="z-index: 1; left: 63px; top: -120px; position: absolute"></asp:Label>
+            &nbsp;<img src="Images/Login.png" alt="Alternate Text" class="logo" /><h2>Log In Here</h2>
+            <asp:Label Text="Email" CssClass="lblEmail" runat="server" />
+            <asp:TextBox ID="txtEmail" runat="server" CssClass="txtEmail" placeholder="Enter Email"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvEmail" runat="server" style="left: 89px; top: 126px;" ErrorMessage="Please provide your email" ControlToValidate="txtEmail" CssClass="validators" SetFocusOnError="True"></asp:RequiredFieldValidator>
+            <asp:RegularExpressionValidator ID="revEmail" runat="server" ErrorMessage="Invalid Email Format" ControlToValidate="txtEmail" style="left: 89px; top: 126px;" CssClass="validators" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" SetFocusOnError="True"></asp:RegularExpressionValidator>
+            <asp:Label Text="Password" CssClass="lblPassword" runat="server" />
+            <asp:RequiredFieldValidator ID="rfvPassword" runat="server" ErrorMessage="Please provide password" ControlToValidate="txtPassword" CssClass="validators" SetFocusOnError="True"></asp:RequiredFieldValidator>
+            <asp:TextBox ID="txtPassword" runat="server" CssClass="txtPassword" placeholder="********" TextMode="Password"/>
+            <asp:Button ID="btnSubmit" Text="Sign In" CssClass="btnSubmit" runat="server" OnClick="btnSubmit_Click" />
+            <asp:Button ID="btnCancel" Text="Back" CssClass="btnSubmit" runat="server" CausesValidation="false" OnClick="btnCancel_Click" />
+            <asp:LinkButton Text="Forgot Password?" CssClass="btnForget" runat="server" OnClick="btnForgotPassword_Click" CausesValidation="false"/>
+        </form>
+    </div>
+</body>
+</html>
---
+++ MemShare/Login.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace MemShare
+{
+    public partial class Login: System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Cookies.Remove("isValid");
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string password;
+                string sql;
+                string conStr = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+
+                SqlConnection con = new SqlConnection(conStr);
+
+                sql = @"SELECT Passwords FROM tblUsers WHERE Email = '" + txtEmail.Text + "'";
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                password = txtPassword.Text;
+
+                con.Open();
+
+                string quaryresult = Convert.ToString(cmd.ExecuteScalar());
+
+                if (quaryresult != null)
+                {
+                    if (quaryresult.Equals(password))
+                    {
+                        sql = @"SELECT Roles FROM tblUsers WHERE Email = '" + txtEmail.Text + "'";
+                        cmd =  new SqlCommand(sql, con);
+
+                        string resultRole = Convert.ToString(cmd.ExecuteScalar());
+
+                        HttpCookie isValid = new HttpCookie("isValid");
+                        isValid["valid"] = txtEmail.Text;
+                        Response.Cookies.Add(isValid);
+                        isValid.Expires = DateTime.Now.AddMinutes(30);
+
+                        if (resultRole.Equals("U"))
+                        {
+                            Session["role"] = resultRole;
+                            Session["email"] = txtEmail.Text;
+                            Session["password"] = txtPassword.Text;
+
+                            cmd.Dispose();
+                            con.Close();
+
+                            Response.Redirect("Home.aspx");
+                        }
+                        else
+                        {
+                            Session["role"] = resultRole;
+                            Session["email"] = txtEmail.Text;
+                            Session["password"] = txtPassword.Text;
+
+                            cmd.Dispose();
+                            con.Close();
+
+                            Response.Redirect("AdminAbout.aspx");
+                        }
+
+                    }
+                    else
+                    {
+                        lblError.Text = "Log in credentials do not match.<br /> Please try again";
+                        lblError.Visible = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "Log in credentials do not match.<br /> Please try again";
+                lblError.Visible = true;
+            }
+        }
+
+        protected void btnForgotPassword_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RequestCode.aspx");
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Welcome.aspx");
+        }
+    }
+}
